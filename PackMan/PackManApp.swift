43e6dc1f6042624e9b97c102316c86dc0df6c99e
dkrymlov <91798748+dkrymlov@users.noreply.github.com>
2023-10-02 16:56:58 +0300

    Initial Commit

---
+++ PackMan/PackManApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackManApp.swift
+//  PackMan
+//
+//  Created by Даниил Крымлов on 02.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct PackManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
